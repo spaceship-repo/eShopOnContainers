0504cd4c0589c9695bd1a5e4111395414510e958
cuongnguyenm3 <cuongnguyenm3@VNNOT01174>
2022-09-08 20:47:10 +0700

    update

--- src/Web/WebSPA/Client/src/modules/basket/basket.service.ts
+++ src/Web/WebSPA/Client/src/modules/basket/basket.service.ts
@@ -29,10 +29,8 @@ export class BasketService {
 
     constructor(private service: DataService, private authService: SecurityService, private basketWrapperService: BasketWrapperService, private router: Router, private configurationService: ConfigurationService, private storageService: StorageService) {
         this.basket.items = [];
-        this.basketUrl = this.configurationService.serverSettings.purchaseUrl;
-        this.purchaseUrl = this.configurationService.serverSettings.purchaseUrl;
-        this.basket.buyerId = this.authService?.UserData?.sub;
         // Init:
+        console.log('this.authService ', this.authService)
         if (this.authService.IsAuthorized) {
             if (this.authService.UserData) {
                 this.basket.buyerId = this.authService.UserData.sub;
