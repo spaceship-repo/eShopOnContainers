00f19bc74e4f582874532e9037e51a58fe01e76f
Your Name <you@example.com>
2022-10-05 07:18:31 +0700

    update

--- src/Web/WebSPA/Client/src/modules/shared/services/configuration.service.ts
+++ src/Web/WebSPA/Client/src/modules/shared/services/configuration.service.ts
@@ -16,31 +16,18 @@ export class ConfigurationService {
     constructor(private http: HttpClient, private storageService: StorageService) { }
 
     load() {
-        setTimeout(() => {
-            const baseURI = document.baseURI.endsWith('/') ? document.baseURI : `${document.baseURI}/`;
-            let url = `${baseURI}Home/Configuration`;
-            this.http.get(url).subscribe((response) => {
-                console.log('server settings loaded');
-                this.serverSettings = response as IConfiguration;
-                console.log(this.serverSettings);
-                this.storageService.store('identityUrl', this.serverSettings.identityUrl);
-                this.storageService.store('purchaseUrl', this.serverSettings.purchaseUrl);
-                this.storageService.store('signalrHubUrl', this.serverSettings.signalrHubUrl);
-                this.storageService.store('activateCampaignDetailFunction', this.serverSettings.activateCampaignDetailFunction);
-                this.isReady = true;
-                this.settingsLoadedSource.next();
-            });
-        }, 1000);
-       
-            //setTimeout(() => {
-            //    this.serverSettings = JSON.parse("{\r\n    \"identityUrl\": \"http:\\/\\/host.docker.internal:5105\",\r\n    \"callBackUrl\": \"http:\\/\\/host.docker.internal:5104\\/\",\r\n    \"basketUrl\": \"http:\\/\\/host.docker.internal:5103\",\r\n    \"purchaseUrl\": \"http:\\/\\/host.docker.internal:5202\",\r\n    \"purchaseUrlHc\": \"http:\\/\\/host.docker.internal:5202\\/hc\",\r\n    \"identityUrlHc\": \"http:\\/\\/host.docker.internal:5105\\/hc\",\r\n    \"signalrHubUrl\": \"http:\\/\\/host.docker.internal:5112\",\r\n    \"useCustomizationData\": true,\r\n    \"isClusterEnv\": \"False\",\r\n    \"activateCampaignDetailFunction\": false,\r\n    \"logging\": {\r\n        \"includeScopes\": false,\r\n        \"logLevel\": {\r\n            \"default\": \"Debug\",\r\n            \"system\": \"Information\",\r\n            \"microsoft\": \"Information\"\r\n        }\r\n    },\r\n    \"applicationInsights\": {\r\n        \"instrumentationKey\": \"\"\r\n    }\r\n}") as IConfiguration;
-            //    this.storageService.store('identityUrl', 'http://host.docker.internal:5105');
-            //    this.storageService.store('purchaseUrl', 'http://host.docker.internal:5202');
-            //    this.storageService.store('signalrHubUrl', 'http://host.docker.internal:5112');
-            //    this.storageService.store('activateCampaignDetailFunction', false);
-            //    this.isReady = true;
-            //    this.settingsLoadedSource.next();
-            //}, 1000);
-            
+        const baseURI = document.baseURI.endsWith('/') ? document.baseURI : `${document.baseURI}/`;
+        let url = `${baseURI}Home/Configuration`;
+        this.http.get(url).subscribe((response) => {
+            console.log('server settings loaded');
+            this.serverSettings = response as IConfiguration;
+            console.log(this.serverSettings);
+            this.storageService.store('identityUrl', this.serverSettings.identityUrl);
+            this.storageService.store('purchaseUrl', this.serverSettings.purchaseUrl);
+            this.storageService.store('signalrHubUrl', this.serverSettings.signalrHubUrl);
+            this.storageService.store('activateCampaignDetailFunction', this.serverSettings.activateCampaignDetailFunction);
+            this.isReady = true;
+            this.settingsLoadedSource.next();
+        });
     }
 }
